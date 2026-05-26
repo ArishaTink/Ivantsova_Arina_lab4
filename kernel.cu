7eab6f1feb1ff3f893a7815a8cb51bfc2bac044c
ArishaTink <dixxan555@gmail.com>
2026-05-26 09:28:04 +0300

    task 1

--- kernel.cu
+++ kernel.cu
@@ -1,144 +1,76 @@
 ﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-#define WIDTH 2024
-#define HEIGHT 2024
-#define BLOCK_SIZE 16
+cudaError_t mulMatricesGPU(int *c, const int *a, const int *b, unsigned int size, int N, clock_t *timer);
 
-cudaError_t blurCuda(unsigned char* input, unsigned char* output, int width, int height, int size, clock_t* timer);
-
-__global__ void blurGPU(unsigned char* input, unsigned char* output, int w, int h)
+__global__ void addKernel(int *c, const int *a, const int *b, int N)
 {
-    __shared__ unsigned char tile[BLOCK_SIZE + 2][BLOCK_SIZE + 2];
-
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
-
-    int tx = threadIdx.x + 1;
-    int ty = threadIdx.y + 1;
-
-    if (row < h && col < w)
-        tile[ty][tx] = input[row * w + col];
-    else
-        tile[ty][tx] = 0;
-
-    if (threadIdx.x == 0) {
-        if (col > 0 && row < h)
-            tile[ty][0] = input[row * w + (col - 1)];
-        else
-            tile[ty][0] = 0;
-    }
-
-    if (threadIdx.x == blockDim.x - 1 || col == w - 1)
-    {
-        if (col + 1 < w && row < h)
-            tile[ty][tx + 1] = input[row * w + (col + 1)];
-
-        else
-            tile[ty][tx + 1] = 0;
-    }
-
-    if (threadIdx.y == 0) {
-        if (row > 0 && col < w)
-            tile[0][tx] = input[(row - 1) * w + col];
-        else
-            tile[0][tx] = 0;
-    }
-
-    if (threadIdx.y == blockDim.y - 1 || row == h - 1) {
-        if (row + 1 < h && col < w)
-            tile[ty + 1][tx] = input[(row + 1) * w + col];
-        else
-            tile[ty + 1][tx] = 0;
-    }
-
-    if (threadIdx.x == 0 && threadIdx.y == 0) {
-        tile[0][0] = (row > 0 && col > 0) ? input[(row - 1) * w + (col - 1)] : 0;
-    }
-
-    if (threadIdx.x == blockDim.x - 1 && threadIdx.y == 0) {
-        tile[0][tx + 1] = (row > 0 && col + 1 < w) ? input[(row - 1) * w + (col + 1)] : 0;
-    }
-
-    if (threadIdx.x == 0 && threadIdx.y == blockDim.y - 1) {
-        tile[ty + 1][0] = (row + 1 < h && col > 0) ? input[(row + 1) * w + (col - 1)] : 0;
-    }
-    if (threadIdx.x == blockDim.x - 1 && threadIdx.y == blockDim.y - 1) {
-        tile[ty + 1][tx + 1] = (row + 1 < h && col + 1 < w) ? input[(row + 1) * w + (col + 1)] : 0;
-    }
-
-    __syncthreads();
-
-    if (row < h && col < w)
-    {
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if (row < N && col < N) {
         int sum = 0;
-        int count = 0;
-
-        for (int dy = -1; dy <= 1; dy++) {
-            for (int dx = -1; dx <= 1; dx++) {
-                int ny = row + dy;
-                int nx = col + dx;
-
-                if (ny >= 0 && ny < h && nx >= 0 && nx < w) {
-                    sum += tile[ty + dy][tx + dx];
-                    count++;
-                }
-            }
+        for (int k = 0; k < N; k++) {
+            sum += a[row * N + k] * b[k * N + col];
         }
-        output[row * w + col] = (unsigned char)(sum / count);
+        c[row * N + col] = sum;
     }
 }
 
-void blurFilterCPU(unsigned char* input, unsigned char* output, int width, int height) {
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
+void mulMatricesCPU(int* c, const int* a, const int* b, int N) {
+    for (int row = 0; row < N; row++) 
+        for (int col = 0; col < N; col++) {
             int sum = 0;
-            int count = 0;
-
-            for (int dy = -1; dy <= 1; dy++) {
-                for (int dx = -1; dx <= 1; dx++) {
-                    int ny = row + dy;
-                    int nx = col + dx;
-
-                    if (ny >= 0 && ny < height && nx >= 0 && nx < width) {
-                        sum += input[ny * width + nx];
-                        count++;
-                    }
-                }
-            }
-
-            output[row * width + col] = (unsigned char)(sum / count);
+            for (int k = 0; k < N; k++) 
+                sum += a[row * N + k] * b[k * N + col];
+            c[row * N + col] = sum;
         }
-    }
 }
 
 int main()
 {
-    int size = WIDTH * HEIGHT * sizeof(unsigned char);
-
-    unsigned char* h_in = (unsigned char*)malloc(size);
-    unsigned char* h_out_cpu = (unsigned char*)malloc(size);
-    unsigned char* h_out_gpu = (unsigned char*)malloc(size);
+    int N;
+    printf("Enter the N*N matrix size: ");
+    scanf("%d", &N);
+    int size = N * N * sizeof(float);
+    int *a = (int*)malloc(size);
+    int *b = (int*)malloc(size);
+    int *c_gpu = (int*)malloc(size);
+    int* c_cpu = (int*)malloc(size);
 
     srand((unsigned)time(NULL));
-    for (int i = 0; i < WIDTH * HEIGHT; i++) {
-        h_in[i] = (unsigned char)(rand() % 256);
+    for (int i = 0; i < N * N; i++) {
+        a[i] = rand() % 10;
+        b[i] = rand() % 10;
     }
 
-    clock_t start = clock();
-    blurFilterCPU(h_in, h_out_cpu, WIDTH, HEIGHT);
-    double cpu_time = (double)(clock() - start) / CLOCKS_PER_SEC;
+    clock_t timer;
 
-    cudaError_t cudaStatus = blurCuda(h_in, h_out_gpu, WIDTH, HEIGHT, size, &start);
+    cudaError_t cudaStatus = mulMatricesGPU(c_gpu, a, b, size, N, &timer);
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
+        fprintf(stderr, "mulMatricesGPU failed!");
         return 1;
     }
-    double
-        gpu_time = (double)(clock() - start) / CLOCKS_PER_SEC;
+    double gpu_time = (double)(clock() - timer) / CLOCKS_PER_SEC;
+
+    timer = clock();
+    mulMatricesCPU(c_cpu, a, b, N);
+    double cpu_time = (double)(clock() - timer) / CLOCKS_PER_SEC;
+
+    int match = 1;
+    for (int i = 0; i < N * N; i++) {
+        if (c_gpu[i] != c_cpu[i]) {
+            match = 0;
+            break;
+        }
+    }
+
+    printf("GPU: %.4f s\n", gpu_time);
+    printf("CPU: %.4f s\n", cpu_time);
+    printf("The results %s\n", match ? "match!" : " DON'T match!");
 
     cudaStatus = cudaDeviceReset();
     if (cudaStatus != cudaSuccess) {
@@ -146,29 +78,14 @@ int main()
         return 1;
     }
 
-    int match = 1;
-    for (int i = 0; i < WIDTH * HEIGHT; i++) {
-        if (h_out_cpu[i] != h_out_gpu[i]) {
-            match = 0;
-            break;
-        }
-    }
-
-    printf("Image resolution: %dx%d\n", WIDTH, HEIGHT);
-    printf("CPU: %.4f s\n", cpu_time);
-    printf("GPU: %.4f s\n", gpu_time);
-    printf("The results %s\n", match ? "match!" : "DON'T match!");
-
-    free(h_in);
-    free(h_out_cpu);
-    free(h_out_gpu);
-
     return 0;
 }
 
-cudaError_t blurCuda(unsigned char* input, unsigned char* output, int width, int height, int size, clock_t* timer)
+cudaError_t mulMatricesGPU(int *c, const int *a, const int *b, unsigned int size, int N, clock_t *timer)
 {
-    unsigned char* d_in, * d_out;
+    int *dev_a = 0;
+    int *dev_b = 0;
+    int *dev_c = 0;
     cudaError_t cudaStatus;
 
     cudaStatus = cudaSetDevice(0);
@@ -177,51 +94,64 @@ cudaError_t blurCuda(unsigned char* input, unsigned char* output, int width, int
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void**)&d_in, size);
+    cudaStatus = cudaMalloc((void**)&dev_c, size);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void**)&d_out, size);
+    cudaStatus = cudaMalloc((void**)&dev_a, size);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMemcpy(d_in, input, size, cudaMemcpyHostToDevice);
+    cudaStatus = cudaMalloc((void**)&dev_b, size);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_a, a, size, cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
-    dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 blocks((WIDTH + BLOCK_SIZE - 1) / BLOCK_SIZE, (HEIGHT + BLOCK_SIZE - 1) / BLOCK_SIZE);
-
-    *timer = clock();
-    blurGPU <<<blocks, threads>>> (d_in, d_out, WIDTH, HEIGHT);
-
-    cudaStatus = cudaGetLastError();
+    cudaStatus = cudaMemcpy(dev_b, b, size, cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "blurGPU launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
+    dim3 threads(16, 16);
+    dim3 blockDim((N + 15) / 16, (N + 15) / 16);
+
+    *timer = clock();
+    addKernel<<<blockDim, threads>>>(dev_c, dev_a, dev_b, N);
+
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
     cudaStatus = cudaDeviceSynchronize();
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
         goto Error;
     }
 
-    cudaStatus = cudaMemcpy(output, d_out, size, cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(c, dev_c, size, cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
 Error:
-    cudaFree(d_in);
-    cudaFree(d_out);
-
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    
     return cudaStatus;
 }
